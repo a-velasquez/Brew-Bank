c072f5cb3d4f033eaccbcd2f0e537ad90e519c62
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-27 19:26:51 -0700

    change type to :brewer and add belongs_to :category

--- db/migrate/20200727170802_create_beers.rb
+++ db/migrate/20200727170802_create_beers.rb
@@ -2,10 +2,10 @@ class CreateBeers < ActiveRecord::Migration[6.0]
   def change
     create_table :beers do |t|
       t.string :name
-      t.string :type
+      t.string :brewer
       t.float :alcohol
       t.text :description
-      t.belongs_to :brewer, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
     end
