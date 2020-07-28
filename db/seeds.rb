4855cf61e3145036a3ac289837e011b4a2e01b22
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-28 11:30:18 -0700

    seed db with user, beer, review and category.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+beer = Beer.create(
+  name: "Bud Light",
+  brewer: "Budweiser",
+  alcohol: 5.0,
+  description: "Bud light is an American light lager beer.",
+  category_id: 2
+)
+
+review = Review.create(
+  title: "Bud Light Review",
+  rating: 3,
+  content: "This beer is cheap and readily available, yet lacks flavor.",
+  user_id: 1,
+  beer_id: 1
+)
