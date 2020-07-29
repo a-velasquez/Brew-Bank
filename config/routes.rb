3938a165175eff9dd5989a9ebb591dae303b8f95
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-29 11:03:35 -0700

    reconfigure routes to use :users resources instead of custome route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/signup' => 'users#new'
+  
   root 'static#home'
-
+  resources :users, only: [:new, :create, :show]
   resources :beers 
   resources :categories
-  resources :users, only: [:show]
+  
 
 end
