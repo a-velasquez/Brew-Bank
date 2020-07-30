bd47a7def6bc27818519e363bf85844d43df29fc
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-30 15:50:32 -0700

    add sessions_controller, new, create, destroy actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController 
+
+  def new 
+  end
+
+  def create 
+    @user = User.find_by(:username: params[:user][:username])
+    if @user && @user.authenticate(password: params[:user][:password])
+      session[:user_id] = @user.id 
+      flash[:message] = "Welcome Back!"
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Sorry, your info was incorrect. Please Try Again."
+      redirect_to login_path
+    end
+  end
+
+  def destroy 
+    #logout 
+  end
+
+end
