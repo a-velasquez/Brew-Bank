459a535c1185f855a8a80eed8fb2097ae553c679
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-30 16:09:00 -0700

    change root route to sessions#welcome

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   
-  root 'static#home'
+  root 'sessions#welcome'
   resources :users, only: [:new, :create, :show]
   resources :beers 
   resources :categories
