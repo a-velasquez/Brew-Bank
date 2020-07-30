e06a080fba8bd267efbcd646c6dffe72aac01fb8
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-30 15:17:19 -0700

    add validations for username, passwd, email to User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :reviews 
   has_many :beers, through: :reviews
-  
+
   has_secure_password
+
+  validates :username, presence: true
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  validates :email, presence: true
+  validates :email, :uniqueness => {:case_sensitive => false}
 end
