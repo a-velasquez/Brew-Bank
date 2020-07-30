99187eb03d2e07291e51e878608f99187faba986
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-30 16:07:12 -0700

    move welcome action to sessions_controller, fix typo in create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController 
 
+  def welcome 
+  end
+
   def new 
   end
 
   def create 
-    @user = User.find_by(:username: params[:user][:username])
+    @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(password: params[:user][:password])
-      session[:user_id] = @user.id 
-      flash[:message] = "Welcome Back!"
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       flash[:message] = "Sorry, your info was incorrect. Please Try Again."
