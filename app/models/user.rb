a8e1de14846ab5344cadcf32cdb3b4e7610551af
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-03 15:01:15 -0700

    add creat_from_omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews 
   has_many :beers, through: :reviews
-
-  has_secure_password
-
   validates :username, presence: true
   validates :username, uniqueness: true 
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   validates :email, presence: true
   validates :email, :uniqueness => {:case_sensitive => false}
+
+  #method to login user using omniauth
+
+  def self.create_from_omniauth(auth)
+    User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+      u.username = auth['info']['first_name']
+      u.email = auth['info']['email']
+      u.password = SecureRandom.hex(16)
+    end
+  end
+
 end
