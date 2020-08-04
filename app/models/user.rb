c5e537f507bab520598a2f3bc26bd65be32bc22c
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-03 23:07:43 -0700

    refactor create_from_omniauth method to work dynamically

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,19 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews 
   has_many :beers, through: :reviews
-  validates :username, presence: true
-  validates :username, uniqueness: true 
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  validates :username, presence: true, uniqueness: true 
   validates :email, presence: true
-  validates :email, :uniqueness => {:case_sensitive => false}
 
-  #method to login user using omniauth
 
+  # method to login user using omniauth
+  # currently works with facebook and google omniauth
   def self.create_from_omniauth(auth)
     User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-      u.username = auth['info']['first_name']
+      u.username = auth['info']['name']
       u.email = auth['info']['email']
       u.password = SecureRandom.hex(16)
     end
   end
+
 
 end
