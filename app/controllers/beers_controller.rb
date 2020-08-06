84cd19be05b57ba38eac3969cdd934f2e4c540f9
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-05 21:17:09 -0700

    add new and index actions

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,7 +1,15 @@
 class BeersController < ApplicationController
+
+  def new 
+    @beer = Beer.new 
+  end
+
+  def index 
+    @beers = Beer.all 
+  end
   
   def show
-    @beer = Beer.first 
+    @beer = Beer.find_by_id(params[:id]) 
   end
 
 
