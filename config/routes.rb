fb6474411ed8c8bc4fc8492b853738d81d1b3874
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-06 12:42:17 -0700

    add reviews show route to nested beer/reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :reviews 
   resources :users, only: [:new, :create, :show]
   resources :beers do
-    resources :reviews, only: [:new, :index]
+    resources :reviews, only: [:new, :index, :show]
   end
   resources :categories
   
