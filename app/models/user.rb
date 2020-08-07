331f76fab3d74b956325a9d11cab6ba24de573a1
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-06 19:40:33 -0700

    add depended destroy to has_many reviews in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :reviews 
-  has_many :beers, through: :reviews
+  has_many :reviews, dependent: :destroy  
+  has_many :beers, through: :reviews, dependent: :destroy  
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true
 
