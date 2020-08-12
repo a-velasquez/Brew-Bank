6a1f7a7d4bd96781040d39165e3feb87a7b89234
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-12 11:01:44 -0700

    add create action, private method to whitelist attributes and build_category for new_beer form

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,8 +2,18 @@ class BeersController < ApplicationController
 
   def new 
     @beer = Beer.new 
+    @beer.build_category
   end
 
+  def create 
+    @beer = Beer.new(beer_params)
+    if @beer.save 
+      redirect_to beer_path(@beer)
+    else
+      render :new 
+    end
+  end
+  
   def index 
     @beers = Beer.all 
   end
@@ -12,5 +22,10 @@ class BeersController < ApplicationController
     @beer = Beer.find_by_id(params[:id]) 
   end
 
+  private 
+
+  def beer_params
+    params.require(:beer).permit(:name, :brewer, :alcohol, :description, :category_id)
+  end
 
 end
