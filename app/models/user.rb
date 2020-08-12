9fec5be56828014955194bfd0722f10b76fd42d4
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-12 11:02:48 -0700

    remove username validations for testing purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews, dependent: :destroy  
   has_many :beers, through: :reviews, dependent: :destroy  
-  validates :username, presence: true, uniqueness: true 
-  validates :email, presence: true
+  validates :username, uniqueness: true 
+  validates :email, uniqueness: true 
 
 
   # method to login user using omniauth
-  # currently works with facebook and google omniauth
+  # currently works with facebook google omniauth
   def self.create_from_omniauth(auth)
     User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
       u.username = auth['info']['name']
@@ -15,6 +15,4 @@ class User < ApplicationRecord
       u.password = SecureRandom.hex(16)
     end
   end
-
-
 end
