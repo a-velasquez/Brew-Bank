b304c4643649e752bbd278347a19ddb559c9a932
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-15 13:29:00 -0700

    add method to redirect user if !logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :authorized?
+  helper_method :current_user, :logged_in?, :authorized?, :redirect_if_not_logged_in
 
   private 
 
@@ -8,13 +8,16 @@ class ApplicationController < ActionController::Base
     !!session[:user_id]
   end
 
-
   def current_user 
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def authorized?(review)
     review.user == current_user
+  end
+
+  def redirect_if_not_logged_in 
+    redirect_to login_path if !logged_in?
   end
 
 end
