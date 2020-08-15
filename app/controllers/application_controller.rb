4690eeb39e257be19a18992ee43e749f8b314c2f
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-15 15:24:15 -0700

    change redirect method to root path from login_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_if_not_logged_in 
-    redirect_to login_path if !logged_in?
+    redirect_to root_path if !logged_in?
   end
 
 end
