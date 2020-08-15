b2bd513ce9a60e9c244d95d8543f4bae6e0caeda
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-15 15:22:51 -0700

    add category_attributes writer to beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,4 +3,10 @@ class Beer < ApplicationRecord
   
   has_many :reviews
   has_many :users, through: :reviews 
+
+  def category_attributes=(attr)     
+    if !attr[:name].blank?
+      self.category = Category.find_or_create_by(name: attr[:name])
+    end
+  end
 end
