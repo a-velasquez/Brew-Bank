77028dda8d44f90ced0cbec49aa871e2a862c1fc
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-16 16:44:40 -0700

    add validation of beer attributes to beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,8 @@ class Beer < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews 
 
+  validates :name, :brewer, :alcohol, :description, presence: true
+
   def category_attributes=(attr)     
     if !attr[:name].blank?
       self.category = Category.find_or_create_by(name: attr[:name])
