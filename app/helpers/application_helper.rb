64a63d4b4c10244f321fc6d8cfdf3be2dfbe291d
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-17 14:10:49 -0700

    add helper to render navigation links based on user being logged in or out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def navigation_links
+    if logged_in?
+      render partial: 'layouts/logged_in'
+    else
+      render partial: 'layouts/logged_out'
+    end
+  end
+
 end
