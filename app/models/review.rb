8ae15db11dd87b520afda353280461b76e95b139
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-16 21:27:17 -0700

    add validations to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :beer
 
-  validates :title, :content, presence: true
+  validates :title, :rating, :content, presence: true
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6} #allows only numbers from 1 - 5 for ratings
+  validates :beer, uniqueness: {scope: :user} #does not allow duplicates of beers in reviews
 end
+  
