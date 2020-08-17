2c44cd730f109c527316b9285b6c8f01c59f1c99
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-16 21:27:03 -0700

    add name validation and method to display brewer and name in dropdown select

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,10 +5,15 @@ class Beer < ApplicationRecord
   has_many :users, through: :reviews 
 
   validates :name, :brewer, :alcohol, :description, presence: true
+  validates :name, uniqueness: true 
 
   def category_attributes=(attr)     
     if !attr[:name].blank?
       self.category = Category.find_or_create_by(name: attr[:name])
     end
+  end
+
+  def name_and_brewer #displays beer brewer and beer name in collection_select dropdown
+    "#{brewer} - #{name}"
   end
 end
