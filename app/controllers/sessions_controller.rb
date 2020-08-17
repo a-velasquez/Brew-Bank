a0e41f7bf9dbc215fa2187ee83288f5346a9ef65
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-16 22:50:46 -0700

    remove pry and refactor sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
-  def welcome 
+
+  def welcome
   end
 
   def new 
@@ -14,13 +15,12 @@ class SessionsController < ApplicationController
       redirect_to user_path(@user)
     else
       flash[:error] = "Sorry, login info was incorrect. Please try again."
-      redirect_to login_path
+      redirect_if_not_logged_in
     end
   end
 
   def omniauth  #currently creates from google and facebook
     user = User.create_from_omniauth(auth)
-    binding.pry
     if user.valid?
       session[:user_id] = user.id
       redirect_to user_path(user)
