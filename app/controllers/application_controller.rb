0159351e93108e373b670a09e3bd58a175cc2be1
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-16 22:41:44 -0700

    add helper method to redirect user from welcome page if already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :authorized?, :redirect_if_not_logged_in
+  helper_method :current_user, :logged_in?, :authorized?, :redirect_if_not_logged_in, :redirect_if_logged_in 
 
   private 
 
@@ -14,6 +14,14 @@ class ApplicationController < ActionController::Base
 
   def authorized?(review)
     review.user == current_user
+  end
+
+  def redirect_if_logged_in 
+    if logged_in? 
+      redirect_to user_path(current_user)
+    else
+      render :welcome
+    end
   end
 
   def redirect_if_not_logged_in 
