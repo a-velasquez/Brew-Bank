4e99fd6cb4b7233d59dec763c7904e130a914967
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-19 12:12:24 -0700

    clean up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password 
+
   has_many :reviews, dependent: :destroy  
   has_many :beers, through: :reviews, dependent: :destroy  
   validates :username, uniqueness: true 
   validates :email, uniqueness: true 
 
-
   # method to login user using omniauth
-  # currently works with facebook google omniauth
   def self.create_from_omniauth(auth)
     User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
       u.username = auth['info']['name']
