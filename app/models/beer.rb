5472476bfc95c2f83c78bdadff0b007ce3efbd5a
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-19 12:12:56 -0700

    add validation for beer name and destroy dependents

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,10 +2,10 @@ class Beer < ApplicationRecord
   belongs_to :category
   
   has_many :reviews
-  has_many :users, through: :reviews 
+  has_many :users, through: :reviews, dependent: :destroy  
 
   validates :name, :brewer, :alcohol, :description, presence: true
-  validates :name, uniqueness: true 
+  validates :name, :uniqueness => { :case_sensitive => false } 
 
   def category_attributes=(attr)     
     if !attr[:name].blank?
