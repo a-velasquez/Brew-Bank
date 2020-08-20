7363011f07026c014f54ae74eeb9e30ca8a6fc77
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-20 01:25:01 -0700

    change beer name validation to include message that beer hjas already been taken

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,11 +1,12 @@
 class Beer < ApplicationRecord
   belongs_to :category
+  belongs_to :user 
   
   has_many :reviews
   has_many :users, through: :reviews, dependent: :destroy  
 
   validates :name, :brewer, :alcohol, :description, presence: true
-  validates :name, :uniqueness => { :case_sensitive => false } 
+  validates :name, uniqueness: { message: "has already been added." } 
 
   def category_attributes=(attr)     
     if !attr[:name].blank?
