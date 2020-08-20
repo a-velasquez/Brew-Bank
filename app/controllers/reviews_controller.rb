9061d7c43a378d41ec8be0f49d63aaac8313c76c
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-20 01:23:54 -0700

    add edit and update actions to reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,11 +31,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit 
+    @review = Review.find_by_id(params[:id])
+  end
+
+  def update 
+    @review = Review.find_by_id(params[:id])
+    if @review.update (review_params)
+      redirect_to review_path(@review)
+    else
+      render :edit 
+    end 
+  end
+
   private 
 
   def review_params
     params.require(:review).permit(:title, :rating, :content, :beer_id)
   end
-
-
 end
