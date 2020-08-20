29bbe715fb5b27d96fe985bad4ce90665e8d5eb8
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-20 01:28:55 -0700

    add user_id to beers to allow users to create beers for themselves

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_230236) do
+ActiveRecord::Schema.define(version: 2020_08_19_200319) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_230236) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_beers_on_category_id"
   end
 
