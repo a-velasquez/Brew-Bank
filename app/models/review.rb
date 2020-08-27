9dce3b976e8f76f837cec470f284dfa03912e826
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-26 21:17:41 -0700

    add scope to order reviews by rating of beer and class method to display update time

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,9 +3,13 @@ class Review < ApplicationRecord
   belongs_to :beer
 
   validates :title, :rating, :content, presence: true
-  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6} #allows only numbers from 1 - 5 for ratings
-  validates :beer, uniqueness: {scope: :user, message: "Has Already Been Reviewed By You."} #does not allow duplicates of reviews
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 6 } #allows only numbers from 1 - 5 for ratings
+  validates :beer, uniqueness: { scope: :user, message: "Has Already Been Reviewed By You." } #does not allow duplicates of reviews
 
-  scope :order_by_rating, -> {joins(:beer).group(:id).order('avg(rating) desc')}
+  scope :order_by_rating, -> { joins(:beer).group(:id).order('avg(rating) desc') }
+
+  def posted 
+    self.updated_at.localtime.strftime("%I:%M %p on %m/%d/%Y")
+  end
 end
   
