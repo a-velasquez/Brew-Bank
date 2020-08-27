f401a4a0e088fbf5866a7497d6c2774dd09b07f5
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-26 23:58:45 -0700

    remove scope method from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @reviews = @user.reviews.recent 
+    @reviews = @user.reviews
   end
 
   private 
