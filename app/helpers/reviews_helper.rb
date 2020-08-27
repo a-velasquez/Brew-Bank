54cd6067ccb2b93e01e41a0ecbf51b936941b793
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-27 14:01:24 -0700

    fix formatting

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,7 +2,7 @@ module ReviewsHelper
 
   def reviews_index_header(review)
     if params[:beer_id]
-        content_tag(:div, "Add a Review for #{review.beer.name}")
+      content_tag(:div, "Add a Review for #{review.beer.name}")
     else
       content_tag(:div, "Add a Review")
     end
