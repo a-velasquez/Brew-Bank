0b98ed81a2d07f7b524eeeb84596783b17820e57
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-28 13:43:51 -0700

    refactor navigation links helper, logged out links not needed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
 
   def navigation_links
-    if logged_in?
-      render partial: 'layouts/logged_in'
-    else
-      render partial: 'layouts/logged_out'
-    end
+    render partial: 'layouts/logged_in' if logged_in?
   end
 
 end
