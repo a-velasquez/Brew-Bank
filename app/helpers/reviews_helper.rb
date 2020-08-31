c3de87cc281c77f0c180429e9282621e4e708432
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-08-31 15:07:36 -0700

    add review index header to reviews helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,10 +1,18 @@
 module ReviewsHelper
 
-  def reviews_index_header(review)
+  def review_form_header(review)
     if params[:beer_id]
       content_tag(:div, "Add a Review for #{review.beer.name}")
     else
       content_tag(:div, "Add a Review")
+    end
+  end
+
+  def review_index_header(beer)
+    if params[:beer_id]
+      content_tag(:div, "All Reviews for #{beer.name}") 
+    else
+      content_tag(:div, "All Reviews")
     end
   end
 end
