dafcf74406290b9da42f0f9ec890a0cf662c32fb
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-09-01 22:15:08 -0700

    remove dependent destroy

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,7 +3,7 @@ class Beer < ApplicationRecord
   belongs_to :user 
   
   has_many :reviews
-  has_many :users, through: :reviews, dependent: :destroy  
+  has_many :users, through: :reviews 
 
   validates :name, :brewer, :alcohol, :description, presence: true
   validates :name, uniqueness: { message: "has already been added.", case_sensitive: false } 
