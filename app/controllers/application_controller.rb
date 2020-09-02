b4c876db2f2cdedc13255b88b7e7dffd53578c60
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-09-01 21:51:18 -0700

    add find_user_by_id and authorized? method to check ownership of resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
+  def find_user_by_id
+    params[:user_id] ? @user = User.find(params[:user_id]) : @user = User.find(params[:id])
+  end
+
   def redirect_if_not_logged_in 
     redirect_to root_path if !logged_in?
+  end
+
+  def authorized?
+    if find_user_by_id != current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
