4c8d7b7436789de0aa6f5584876bd73c65ea36ca
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-09-02 16:04:48 -0700

    remove dependent destroy, remove note

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
   has_secure_password 
 
-  has_many :reviews, dependent: :destroy  
+  has_many :reviews 
   has_many :reviewed_beers, through: :reviews, source: :beer  
 
-  has_many :beers, dependent: :destroy
+  has_many :beers
 
   validates :username, uniqueness: true
   validates :username, presence: true, allow_nil: true 
   validates :email, uniqueness: true
   validates :email, presence: true, allow_nil: true 
 
-  # method to login user using omniauth
   def self.create_from_omniauth(auth)
     User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
       u.username = auth['info']['name']
